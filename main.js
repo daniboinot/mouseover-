73a49a845e18f4e8a99a911e85ca7c4e7233c352
daniboinot <152235639+daniboinot@users.noreply.github.com>
2023-12-20 21:17:25 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
 
 
-<body>
-  <canvas id="myCanvas" width="400" height="200"></canvas>
 
-  <script>
-    const canvas = document.getElementById('myCanvas');
-    const ctx = canvas.getContext('2d');
-
-    // Draw red rectangle on the canvas
-    ctx.fillStyle = 'red';
-    ctx.fillRect(50, 50, 200, 100);
-
-    canvas.addEventListener('mousemove', function(event) {
-      const rect = canvas.getBoundingClientRect();
-      const mouseX = event.clientX - rect.left;
-      const mouseY = event.clientY - rect.top;
-
-      // Check if the mouse is inside the red rectangle
-      if (mouseX >= 50 && mouseX <= 250 && mouseY >= 50 && mouseY <= 150) {
-        document.body.style.backgroundColor = 'red';
-      } else {
-        document.body.style.backgroundColor = 'white';
-      }
-    });
-  </script>
-</body>
-</html>
